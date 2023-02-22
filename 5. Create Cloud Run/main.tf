1fcd1b5cc3ddd43968cbe5d5d7bf756392c35365
PatrickDegner <acaer@gmx.net>
2023-02-22 20:13:35 +0100

    Terraform Cloud Run

---
+++ 5. Create Cloud Run/main.tf
@@ -0,0 +1,26 @@
+resource "google_cloud_run_service" "run-app-from-tf" {
+  name = "run-app-from-tf"
+  location = "europe-west3"
+  
+  template {
+    spec {
+      containers {
+        image = "gcr.io/google-samples/hello-app:1.0"
+        # image = "gcr.io/google-samples/hello-app:2.0"
+      }
+    }
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "pub_access" {
+  service = google_cloud_run_service.run-app-from-tf.name
+  location = google_cloud_run_service.run-app-from-tf.location
+  policy_data = data.google_iam_policy.pub-1.policy_data
+}
+
+data "google_iam_policy" "pub-1" {
+  binding {
+    role = "roles/run.invoker"
+    members = [ "allUsers", ]
+  }
+}
